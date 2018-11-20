756f4382c96026a3d3c67213cc80cd1b5a70ad27
peteparkinson <43157092+peteparkinson@users.noreply.github.com>
2018-11-20 13:26:13 -0500

    updated GUI with netbeans

--- GUI.java
+++ GUI.java
@@ -1,127 +1,176 @@
-/************************************************
- * GUI.java
- * 11-16-2018
- * Pete Parkinson
- * 
- * draws GUI for simple inventory tracking system
- * 
- ************************************************/
-
-import java.awt.*;
 import javax.swing.*;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.logging.*;
 
-public class GUI extends JFrame{
-	
-	static JButton submitMatBtn = new JButton("Submit");
+public class GUI extends JFrame {
 
-	static JTextField nameField = new JTextField(15);
-	static JTextField descField = new JTextField(15);
-	static JTextField QOHField  = new JTextField(15);
-	static JTextField costField = new JTextField(15);
-	static JTextField extCostField = new JTextField(15);
+    public GUI() {
+    	newMaterialTab();
+    	projectsTab();
+    }
+            
+    private void projectsTab(){
 
-	JLabel nameLabel = new JLabel("Name: ");
-	JLabel descLabel = new JLabel("Description: ");
-	JLabel QOHLabel  = new JLabel("Qty on hand: ");
-	JLabel costLabel = new JLabel("Cost: ");
-	JLabel extCostLabel = new JLabel("Extended Cost: ");
-	
-	JPanel VMPBottom= new JPanel();
+        plusBtn = new JButton();
+        minusBtn = new JButton();
+    	
+    	
+    }
+    
+    private void newMaterialTab() {
 
-	JPanel blankPanel1 = new JPanel();
-	JPanel blankPanel2 = new JPanel();
-	JPanel blankPanel3 = new JPanel();
-	
-	JLabel placeHolder6 = new JLabel("");
-	
-	//to be verified by user then submitted to DB
-	JLabel finalNameLabel   = new JLabel("");
-	
-	JRadioButton radioButton = new JRadioButton("text here");
-	
-	ButtonGroup btnGroup = new ButtonGroup();
-	
-	JTabbedPane jtp = new JTabbedPane();
-	JScrollPane jsp = new JScrollPane();
+        newMatPane 	 = new JTabbedPane();
+        jPanel1 	 = new JPanel();
+        jPanel2 	 = new JPanel();
+        nameLabel 	 = new JLabel();
+        qtyLabel     = new JLabel();
+        costLabel    = new JLabel();
+        extCostLabel = new JLabel();
+        notesLabel   = new JLabel();
+        jScrollPane1 = new JScrollPane();
+        notesArea 	 = new JTextArea();
+        nameField 	 = new JTextField();
+        qtyField 	 = new JTextField();
+        costField 	 = new JTextField();
+        extCostField = new JTextField();
+        submitMatBtn = new JButton();
 
-	JPanel newMaterialTab = new JPanel();
-	JPanel projectsTab = new JPanel();
-	JPanel newProjectTab = new JPanel();
-	JPanel closedProjectsTab = new JPanel();
+        nameLabel.setText("Name:");
+        qtyLabel.setText("Qty:");
+        costLabel.setText("Cost:");
+        extCostLabel.setText("Ext. Cost:");
+        notesLabel.setText("Notes:");
 
-	
-	JList<Project> ordersList = new JList<>();
-	DefaultListModel<Project> orderModel = new DefaultListModel<Project>();
-	
-	Font font1 = new Font("",Font.BOLD,16);
-	
+        notesArea.setColumns(20);
+        notesArea.setRows(5);
+        jScrollPane1.setViewportView(notesArea);
 
+        submitMatBtn.setText("Submit");
+        submitMatBtn.addActionListener(new Action());
 
-
-	
-	public GUI() {
-		super("Art Projects Manager");
-		//setLayout(new GridLayout(0,2,5,5));
-		newMaterialTab.setLayout (new GridLayout(0,2,5,5));
-		
-		extCostField.setEditable(false);
-		
-        //customerLabel.setFont(font1);
+        extCostField.setEditable(false);
         
-		radioButton.setVisible(false);
-		
-		ordersList.setPreferredSize(new Dimension(150,140));
+        GroupLayout jPanel2Layout = new GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel2Layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGroup(jPanel2Layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanel2Layout.createParallelGroup(GroupLayout.Alignment.TRAILING)
+                                .addGroup(jPanel2Layout.createSequentialGroup()
+                                    .addComponent(nameLabel)
+                                    .addGap(46, 46, 46))
+                                .addGroup(GroupLayout.Alignment.LEADING, jPanel2Layout.createSequentialGroup()
+                                    .addComponent(qtyLabel)
+                                    .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)))
+                            .addGroup(jPanel2Layout.createSequentialGroup()
+                                .addComponent(costLabel)
+                                .addGap(51, 51, 51))
+                            .addComponent(extCostLabel))
+                        .addGroup(jPanel2Layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                            .addComponent(nameField, GroupLayout.DEFAULT_SIZE, 124, Short.MAX_VALUE)
+                            .addComponent(qtyField)
+                            .addComponent(costField)
+                            .addComponent(extCostField))
+                        .addGap(18, 18, 18)
+                        .addComponent(notesLabel)
+                        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jScrollPane1, GroupLayout.PREFERRED_SIZE, 259, GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addComponent(submitMatBtn, GroupLayout.PREFERRED_SIZE, 94, GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap())
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel2Layout.createParallelGroup(GroupLayout.Alignment.LEADING, false)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGroup(jPanel2Layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanel2Layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                                .addComponent(notesLabel)
+                                .addComponent(nameLabel))
+                            .addComponent(nameField, GroupLayout.Alignment.TRAILING, GroupLayout.PREFERRED_SIZE, 28, GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(jPanel2Layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                            .addComponent(qtyLabel)
+                            .addComponent(qtyField, GroupLayout.PREFERRED_SIZE, 28, GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(jPanel2Layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                            .addComponent(costLabel)
+                            .addComponent(costField, GroupLayout.PREFERRED_SIZE, 28, GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(jPanel2Layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                            .addComponent(extCostLabel)
+                            .addComponent(extCostField, GroupLayout.PREFERRED_SIZE, 28, GroupLayout.PREFERRED_SIZE)))
+                    .addComponent(jScrollPane1))
+                .addGap(18, 18, 18)
+                .addComponent(submitMatBtn, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(27, Short.MAX_VALUE))
+        );
 
-		ordersList.setModel(orderModel);
-		ordersList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-				
-		//test data for JList
-		orderModel.addElement(new Project(/* enter parameters here */));
-		
-		btnGroup.add(radioButton);	
+        GroupLayout jPanel1Layout = new GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel2, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
 
-		VMPBottom.add(submitMatBtn);
-		
-		newMaterialTab.add(nameLabel);
-		newMaterialTab.add(nameField);
-		newMaterialTab.add(descLabel);
-		newMaterialTab.add(descField);
-		newMaterialTab.add(QOHLabel);
-		newMaterialTab.add(QOHField);
-		newMaterialTab.add(costLabel);
-		newMaterialTab.add(costField);
-		newMaterialTab.add(extCostLabel);
-		newMaterialTab.add(extCostField);
-		newMaterialTab.add(blankPanel1);
-		newMaterialTab.add(blankPanel2);
-		newMaterialTab.add(blankPanel3);
-		newMaterialTab.add(VMPBottom);
-		
-		/*
-		newMaterialTab.add(new JScrollPane(ordersList));
-		newMaterialTab.add(radioButton);
-		 */
+        newMatPane.addTab("New Material", jPanel1);
 
-		jtp.addTab("New Material", newMaterialTab);
-		jtp.addTab("Projects", projectsTab);
-		jtp.addTab("New Projects", newProjectTab);
-		jtp.addTab("Closed Projects", closedProjectsTab);
-		
-		add(jtp);
+        GroupLayout layout = new GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addComponent(newMatPane)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addComponent(newMatPane, GroupLayout.Alignment.TRAILING)
+        );
 
-		submitMatBtn.addActionListener(new Action());
+        pack();
+    }                    
+    
+    //Materials Tab elements
+    private JLabel nameLabel;
+    private JLabel qtyLabel;
+    private JLabel costLabel;
+    private JLabel extCostLabel;
+    private JLabel notesLabel;
+    private JPanel jPanel1;
+    private JPanel jPanel2;
+    
+    static JTextArea notesArea;
+    static JTextField nameField;
+    static JTextField qtyField;
+    static JTextField costField;
+    static JTextField extCostField;
+    
+    static JButton submitMatBtn;
+    
+    private JScrollPane jScrollPane1;
+    
+    private JTabbedPane newMatPane;
 
-		nameField.addKeyListener(new Action());
-	
-		ordersList.addListSelectionListener(new ListSelectionListener() {
-			public void valueChanged(ListSelectionEvent event){
-				if(event.getSource() == costField || event.getSource() == QOHField) {
-					//calculate extended cost here
-				}
-			}
 
-		});
-	}
+    //Projects Tab elements
+    static JButton plusBtn;
+    static JButton minusBtn;
 }
